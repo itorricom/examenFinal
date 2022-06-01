199bc5a97d77d3fab66cdf38e5f68ceac04a1d72
Isidoro Torrico Mamani <itorrico.m@gmail.com>
2022-06-01 19:41:33 -0400

    Instalando serviceWorker y verificando

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
-const STATIC_CACHE = 'static';
+const STATIC_CACHE = "static";
 
-self.addEventListener("install", e =>{
+const APP_SHELL = [
+    "/",
+    "index.html",
+    "css/style.css",
+    "js/main.js",
+];
 
+self.addEventListener("install", (e) => {
+    const cacheStatic = caches
+    .open(STATIC_CACHE)
+    .then((cache) => cache.addAll(APP_SHELL));
+
+    e.wait.Until(cacheStatic);
+}); 
+
+self.addEventListener("fetch", e => {
+    console.log("fectch! ", e.request);
 })
