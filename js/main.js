d3eeffc1c8da4f79e1126ff2adccd998e1678257
Isidoro Torrico Mamani <itorrico.m@gmail.com>
2022-06-01 21:28:27 -0400

    Corregiendo deltalles para que sea instalable la app ok

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ let swLocation = "sw.js";
 if (navigator.serviceWorker){
     if(window.location.href.includes("localhost")) swLocation = "/sw.js"; //varia segun host
     navigator.serviceWorker.register(swLocation);
-    console.log("Hola service worker 2022");
+    //console.log("Hola service worker 2022");
 }
 //logica de la aplicación web 
 
-//console.log("Hola service worker");
+console.log("Hola service worker");
--- sw.js
+++ sw.js
@@ -1,28 +1,29 @@
-const STATIC_CACHE = "static";
+const STATIC_CACHE = "static-v1";
 
 const APP_SHELL = [
-    "/",
-    "index.html",
-    "css/style.css",
-    "js/main.js",
+  "/",
+  "index.html",
+  "css/styles.css",
+  "js/main.js",
 ];
 
 self.addEventListener("install", (e) => {
-    const cacheStatic = caches
+  const cacheStatic = caches
     .open(STATIC_CACHE)
     .then((cache) => cache.addAll(APP_SHELL));
 
-    e.waitUntil(cacheStatic);
-}); 
+  e.waitUntil(cacheStatic);
+});
 
 self.addEventListener("fetch", (e) => {
-    console.log("fetch! ", e.request);
-    e.respondWith(
-      caches
-        .match(e.request)
-        .then((res) => {
-          return res || fetch(e.request);
-        })
-        .catch(console.log)
-    );
+  console.log("fetch! ", e.request);
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then((res) => {
+        return res || fetch(e.request);
+      })
+      .catch(console.log)
+  );
+  //   e.waitUntil(response);
 });
